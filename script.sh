4dfaa0dbddc58f4e3234e917aac15b96791af5a1
student <student@student.example.local>
2024-05-14 11:05:56 +0200

    script

---
+++ script.sh
@@ -0,0 +1,5 @@
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+wget https://github.com/fn2006/PollyMC/releases/download/8.0/PollyMC-Linux-x86_64.AppImage
+tar -xf jdk-8u131-linux-x64.tar.gz
+chmod 777 *
+./PollyMC-Linux-x86_64.AppImage
