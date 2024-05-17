1ff1fdb7af9e402f8766804f92f6cb59e8845dad
Michael Antoš <128068126+antosmichael07@users.noreply.github.com>
2024-05-17 09:02:24 +0200

    Create java-21.sh

---
+++ java-21.sh
@@ -0,0 +1,3 @@
+wget https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz
+tar -xf openjdk-21.0.2_linux-x64_bin.tar.gz
+chmod 777 *
